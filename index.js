830e5a97ea2567b53c06be474c0b6b7266c1655d
Sang Le <57263682+sqle157@users.noreply.github.com>
2022-03-23 15:59:28 -0600

    fix accessibility issue with form element

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const toggleBtn = document.querySelector('.toggle__btn');
+const toggleBtn = document.querySelector('#toggle__btn');
 
 toggleBtn.addEventListener('click', () => {
 	// console.log(e.target);
