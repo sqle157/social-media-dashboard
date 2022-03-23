fdfd2caad81eee5d74b2913e6978eeabc682a35c
Sang Le <57263682+sqle157@users.noreply.github.com>
2022-03-23 16:09:18 -0600

    fix minor bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.toggle__btn');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', (e) => {
 	// console.log(e.target);
 	document.body.classList.toggle('light-theme');
 });
